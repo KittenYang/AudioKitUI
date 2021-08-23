cfbb33d132df87c6444c5fce6121ed4b920ec013
传骑 <qitaoyang.yqt@alibaba-inc.com>
2021-08-24 01:12:07 +0800

    set audioKit revision

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["AudioKitUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/AudioKit/AudioKit.git", .branch("main"))
+        .package(url: "https://github.com/AudioKit/AudioKit.git", .revision("5f6c03abe8e86905a4d8b23fb54eea86ed00f862"))
     ],
     targets: [
         .target(
